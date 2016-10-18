af4c7f1622c919b066e4cc2a8d02bcb925e3b177
Kabir <kabirbaidhya@gmail.com>
2016-10-18 11:08:37 +0545

    Introduce TodoService for Todo CRUD

---
+++ src/todos/TodoService.js
@@ -0,0 +1,12 @@
+TodoService.$inject = ['$resource'];
+
+export default function TodoService($resource) {
+    const API_BASE = 'http://127.0.0.1:8080';
+
+    let todo = $resource(API_BASE + '/api/todos/:id');
+
+    this.getAll = () => todo.query().$promise;
+    this.get = (id) => todo.get({id: id}).$promise;
+    this.update = (id, data) => todo.update({id: id}, data).$promise;
+    this.delete = (id) => todo.delete({id: id}).$promise;
+}
--- src/todos/TodosController.js
+++ src/todos/TodosController.js
@@ -1,5 +1,25 @@
-import angular from 'angular';
+TodosController.$inject = ['TodoService'];
 
-export default function TodosController() {
-    this.text = 'Hello';
+export default function TodosController(TodoService) {
+    let vm = this;
+
+    vm.todos = [];
+
+    function init() {
+        loadTodos();
+    }
+
+    function loadTodos() {
+        vm.loading = true;
+
+        TodoService.getAll()
+            .then((data) => {
+                vm.todos = data;
+            })
+            .finally(() => {
+                vm.loading = false;
+            });
+    }
+
+    init();
 }
